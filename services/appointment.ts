0ee8b1b8836f94ec2aedb16ff1c605038cf56124
russellsocola <russell999nahum@gmail.com>
2025-08-27 17:54:27 -0500

    Modificacion de la Lambda appointment para la actualizacion de la tabla

--- services/appointment.ts
+++ services/appointment.ts
@@ -1,6 +1,6 @@
-import { APIGatewayProxyHandler } from "aws-lambda";
+import { APIGatewayProxyEvent, SQSEvent } from "aws-lambda";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { PutCommand } from "@aws-sdk/lib-dynamodb";
+import { PutCommand, GetCommand, UpdateCommand } from "@aws-sdk/lib-dynamodb";
 import { SNSClient, PublishCommand } from "@aws-sdk/client-sns"
 
 const client = new DynamoDBClient();
@@ -8,53 +8,101 @@ const clientSns = new SNSClient();
 const TABLE = process.env.REGISTER_TABLE || "";
 const TOPIC_ARN = process.env.TOPIC_ARN || "";
 
-export const handler: APIGatewayProxyHandler = async (event) => {
-    try {
-        if (!event.body) {
-            return { statusCode: 400, body: JSON.stringify({ message: "Body vacio" }) }
-        }
+export const handler = async (event: SQSEvent | APIGatewayProxyEvent) => {
+    if ("Records" in event) {
+        // Viene de SQS
+        for (const record of event?.Records) {
+            console.log("📩 Mensaje SQS:", JSON.parse(record.body));
+            const { insuredId } = JSON.parse(record.body);
 
-        const body = JSON.parse(event.body);
-        const { insuredId, scheduleId, countryISO } = body;
-        if (!insuredId || !scheduleId || !countryISO) {
-            return { statusCode: 400, body: JSON.stringify({ message: "Faltan campos requeridos" }) };
+            const getResult = await client.send(new GetCommand({
+                TableName: TABLE,
+                Key: { id: insuredId },
+            }));
+
+            if (!getResult.Item) {
+                return {
+                    statusCode: 404,
+                    body: JSON.stringify({ message: "Id no encontrado" }),
+                }
+            }
+
+            try {
+                const result = await client.send(
+                    new UpdateCommand({
+                        TableName: TABLE,
+                        Key: { id: insuredId },
+                        UpdateExpression: "SET #st = :newStatus, modifyAt = :now",
+                        ExpressionAttributeNames: {
+                            "#st": "status",
+                        },
+                        ExpressionAttributeValues: {
+                            ":newStatus": "Completed",
+                            ":now": new Date().toISOString(),
+                        },
+                        ReturnValues: "ALL_NEW",
+                    })
+                );
+
+                console.log(result.Attributes)
+
+                return {
+                    statuscode: 200,
+                    body: JSON.stringify({ Message: result })
+                }
+            } catch (error) {
+                console.error("❌ Error actualizando item:", error);
+                throw error;
+            }
         }
-        if (!/^\d{5}$/.test(body.insuredId)) {
-            return { statusCode: 400, body: JSON.stringify({ message: "insuredId debe ser 5 dígitos" }) };
-        }
-        if (typeof body.scheduleId !== "number") {
-            return { statusCode: 400, body: JSON.stringify({ message: "scheduleId debe ser número" }) };
-        }
-        if (!["PE", "CL"].includes(body.countryISO)) {
-            return { statusCode: 400, body: JSON.stringify({ message: "countryISO inválido (PE o CL)" }) };
-        }
+    } else {
+        try {
+            if (!event.body) {
+                return { statusCode: 400, body: JSON.stringify({ message: "Body vacio" }) }
+            }
+
+            const body = JSON.parse(event.body);
+            const { insuredId, scheduleId, countryISO } = body;
+            if (!insuredId || !scheduleId || !countryISO) {
+                return { statusCode: 400, body: JSON.stringify({ message: "Faltan campos requeridos" }) };
+            }
+            if (!/^\d{5}$/.test(body.insuredId)) {
+                return { statusCode: 400, body: JSON.stringify({ message: "insuredId debe ser 5 dígitos" }) };
+            }
+            if (typeof body.scheduleId !== "number") {
+                return { statusCode: 400, body: JSON.stringify({ message: "scheduleId debe ser número" }) };
+            }
+            if (!["PE", "CL"].includes(body.countryISO)) {
+                return { statusCode: 400, body: JSON.stringify({ message: "countryISO inválido (PE o CL)" }) };
+            }
 
 
-        const newItem = {
-            id: insuredId,
-            scheduleId: scheduleId,
-            countryISO: countryISO,
-            createAt: new Date().toISOString(),
-            modifyAt: null
-        }
-        const params = {
-            TableName: TABLE,
-            Item: newItem
-        }
-        await client.send(new PutCommand(params))
-        await clientSns.send(new PublishCommand({ TopicArn: TOPIC_ARN, Message: JSON.stringify(newItem) }))
+            const newItem = {
+                id: insuredId,
+                scheduleId: scheduleId,
+                countryISO: countryISO,
+                createAt: new Date().toISOString(),
+                modifyAt: null
+            }
+            const params = {
+                TableName: TABLE,
+                Item: newItem
+            }
+            await client.send(new PutCommand(params))
+            await clientSns.send(new PublishCommand({ TopicArn: TOPIC_ARN, Message: JSON.stringify(newItem) }))
 
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: "Se creo el evento" })
-        }
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ message: "Se creo el evento" })
+            }
 
-    } catch (error) {
-        console.error("Error:", error)
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ message: "Internal server Error" })
-        };
+        } catch (error) {
+            console.error("Error:", error)
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ message: "Internal server Error" })
+            };
+        }
     }
 
 }
--- services/appointment_pe.ts
+++ services/appointment_pe.ts
@@ -1,8 +1,62 @@
-import { APIGatewayProxyHandler } from "aws-lambda";
+import { EventBridgeClient, PutEventsCommand } from "@aws-sdk/client-eventbridge";
+import { SQSEvent } from "aws-lambda";
+import mysql from "mysql2/promise";
 
-export const handler: APIGatewayProxyHandler = async (event) => {
-    return{
-        statusCode: 200,
-        body: JSON.stringify({message:"Success"})
+const client = new EventBridgeClient({});
+
+const DB_HOST = process.env.DB_HOST || "";
+const DB_USER = process.env.DB_USER || "";
+const DB_PASSWORD = process.env.DB_PASSWORD || "";
+const DB_NAME = process.env.DB_NAME || "";
+
+export const handler = async(event: SQSEvent) => {
+    let connection;
+
+    try {
+        // Conexión al RDS
+        connection = await mysql.createConnection({
+            host: DB_HOST,
+            user: DB_USER,
+            password: DB_PASSWORD,
+            database: DB_NAME,
+        });
+
+        for (const record of event.Records) {
+            const body = JSON.parse(record.body);
+
+            console.log("📩 Mensaje recibido de SQS:", body);
+
+            const { insuredId, scheduleId, countryISO } = body;
+
+            await connection.execute(
+                "INSERT INTO appointments_pe (insured_id, schedule_id, country_iso, created_at) VALUES (?, ?, ?, NOW())",
+                [insuredId, scheduleId, countryISO]
+            );
+
+            const command = new PutEventsCommand({
+                Entries: [
+                    {
+                        Source: "app.citamedica",
+                        DetailType: "AppointmentCreated",
+                        Detail: JSON.stringify(body),
+                        EventBusName: process.env.EVENT_BUS || "default",
+                    },
+                ],
+            });
+
+            const response = await client.send(command);
+
+            console.log("Evento publicado:", response);
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ message: "Evento publicado en EventBridge" }),
+            };
+        }
+    } catch (error) {
+        console.error("Error:", error)
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Internal server Error" })
+        };
     }
 }
